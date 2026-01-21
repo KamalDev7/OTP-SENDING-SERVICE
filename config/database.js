a66ee391cd24424c580d154baa3da95172ec9bbe
KAMAL <kamalsinh0p@gmail.com>
2026-01-21 17:23:39 +0530

    supabase connection string update

--- config/database.js
+++ config/database.js
@@ -14,20 +14,33 @@
 // module.exports = con;
 
 
+// const { Pool } = require("pg");
+
+// const con = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false } // REQUIRED on Render
+// });
+
+// con.on("connect", () => {
+//   console.log("Supabase PostgreSQL connected");
+// });
+
+// con.on("error", (err) => {
+//   console.error(" DB error:", err);
+//   process.exit(1);
+// });
+
+// module.exports = con;
+
 const { Pool } = require("pg");
 
 const con = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // REQUIRED on Render
+  ssl: { rejectUnauthorized: false }
 });
 
 con.on("connect", () => {
   console.log("Supabase PostgreSQL connected");
-});
-
-con.on("error", (err) => {
-  console.error(" DB error:", err);
-  process.exit(1);
 });
 
 module.exports = con;
