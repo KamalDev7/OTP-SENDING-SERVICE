b2eb318b5f1b7771e748adaf88ec49316c3c05e0
KAMAL <kamalsinh0p@gmail.com>
2026-01-22 10:07:28 +0530

    supabase

--- config/database.js
+++ config/database.js
@@ -32,12 +32,34 @@
 
 // module.exports = con;
 
+// const { Pool } = require("pg");
+
+// const con = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false }
+// });
+
+// con.on("connect", () => {
+//   console.log("Supabase PostgreSQL connected");
+// });
+
+// module.exports = con;
+
+
+
 const { Pool } = require("pg");
 
 const con = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  max: 5,
+  connectionTimeoutMillis: 5000,
+  idleTimeoutMillis: 30000,
+  statement_timeout: 5000
 });
+
+// VERY IMPORTANT: disable prepared statements
+con.options = "--disable-prepared-statements";
 
 con.on("connect", () => {
   console.log("Supabase PostgreSQL connected");
