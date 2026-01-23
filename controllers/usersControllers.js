c9a7fc69a69330e9608f2537106ce2d38d1ffc83
KAMAL <kamalsinh0p@gmail.com>
2026-01-23 13:37:54 +0530

    Status column drop from DB

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -19,53 +19,8 @@ exports.getAllUsers = async (req, res) => {
 }
 
 
-exports.getUserStatus = async (req, res) => {
-    try {
-        const { email } = req.params;
 
-        const response = await con.query(
-            "SELECT status FROM users1 WHERE email=$1",
-            [email]
-        );
 
-        if (response.rowCount > 0) {
-            res.status(200).json({
-                status: response.rows[0].status,
-            });
-        }
-        console.log(`Uer ststus:: ${response.rows[0].status}`);
-
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-exports.setUserStatus = async (req, res) => {
-    try {
-        const { email, status } = req.body;
-
-        const response = await con.query(
-            "UPDATE users1 set status=$1 where email=$2",
-            [status, email]
-        );
-
-        if (response.rowCount === 0) {
-            res.status(404).json({
-                message: "User not found"
-            });
-        }
-
-        res.status(200).json({
-            message: "User status updated successfully",
-            email
-        });
-
-        console.log(`User status changed to '${status}', Email: ${email}`);
-
-    } catch (err) {
-        console.log(err);
-    }
-}
 
 exports.changeUserRole = async (req, res) => {
     try {
@@ -96,3 +51,4 @@ exports.changeUserRole = async (req, res) => {
 }
 
 
+// exports.
